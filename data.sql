d89e40a136e97894f46b085c4e099d9b74a833a8
mullenlucas <mullenlucas@gmail.com>
2022-10-26 19:06:44 -0300

    insert data into owners and species tables

--- data.sql
+++ data.sql
@@ -13,3 +13,17 @@ INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
   ('Boarmon', '2005-06-07', 7, true, 20.4),
   ('Blossom', '1998-10-13', 3, true, 17),
   ('Ditto', '2022-05-14', 4, true, 22);
+
+-- Insert data into 'owners' table
+INSERT INTO owners (full_name, age) 
+  VALUES ('Sam Smith', 34),
+  ('Jennifer Orwell', 19),
+  ('Bob', 45),
+  ('Melody Pond', 77),
+  ('Dean Winchester', 14),
+  ('Jodie Whittaker', 38);
+
+-- Insert data into 'species' table
+INSERT INTO species (name) 
+  VALUES ('Pokemon'),
+  ('Digimon');

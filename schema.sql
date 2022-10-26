90e6a4527647809eb35db89858f47ea0c32f78ce
mullenlucas <mullenlucas@gmail.com>
2022-10-26 15:49:27 -0300

    add species to animals table

--- schema.sql
+++ schema.sql
@@ -8,3 +8,6 @@ CREATE TABLE animals (
     neutered BOOLEAN NOT NULL,
     weight_kg DECIMAL NOT NULL
 );
+
+-- Add a column species of type string to your animals table
+ALTER TABLE animals ADD species VARCHAR(60);

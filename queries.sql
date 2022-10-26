21ca76fd03200c0d1c3394e094bf1d66c127ba63
mullenlucas <mullenlucas@gmail.com>
2022-10-26 17:48:45 -0300

    update species to unspecif and rollback

--- queries.sql
+++ queries.sql
@@ -1,4 +1,4 @@
-/*Queries that provide answers to the questions from all projects.*/
+/* Queries */
 
 SELECT * FROM animals WHERE name LIKE '%mon';
 SELECT name FROM animals WHERE date_of_birth BETWEEN DATE '2016-01-01' AND '2019-12-31';
@@ -8,4 +8,19 @@ SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE neutered = 'true';
 SELECT * FROM animals WHERE name != 'Gabumon';
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
-SELECT * FROM animals
+
+-- # TRANSACTIONS #
+
+-- #1
+BEGIN;
+-- setting the species column to unspecified
+UPDATE animals SET species = 'unspecified';
+SELECT species from animals;
+
+-- rollback species column
+ROLLBACK;
+SELECT species from animals;
+
+-- #2
+
+SELECT * FROM animals;
